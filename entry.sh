1a5fa50d03b0205dc0c0ebddfb9bae7b08a9f5f9
emil <emiliansar@gmail.com>
2026-02-27 18:08:14 +0300

    fix entry.sh

--- entry.sh
+++ entry.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# Явно передаем DATABASE_URL из переменных окружения
 export DATABASE_URL="$DATABASE_URL"
-
-# Применяем миграции (если нужно)
 npx prisma migrate deploy
-
-# Запускаем приложение
-node dist/main
+node dist/src/main
